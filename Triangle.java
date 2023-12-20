7e5475c74ac85c2e2368ee0296cfb5893d2dfc41
Aviv Bodeg <AVIVBodeg@gmail.com>
2023-12-20 19:55:08 +0200

    comment, min/max check

--- Triangle.java
+++ Triangle.java
@@ -9,6 +9,7 @@ public class Triangle {
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
 
+		//checks if each side is smaller than the sum of the other two:
 		boolean isTriangle = (a + b > c) && (a + c > b) && (b + c > a);
 		System.out.println(a + ", " + b + ", " + c + ": " + isTriangle);
 	}
