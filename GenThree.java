3342e8547ae73a24a3190e3f1534f0cae3221282
Aviv Bodeg <AVIVBodeg@gmail.com>
2023-12-20 20:10:33 +0200

    GenThree Update

--- GenThree.java
+++ GenThree.java
@@ -3,20 +3,23 @@
  * prints them, and then prints the minimal number that was generated.
  */
 public class GenThree {
-	public static void main(String[] args){
-		double min = Double.parseDouble(args[0]);
-		double max = Double.parseDouble(args[1]);
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
 
-		int gen1 = (int) (Math.random() * (max - min) + min);
-		int gen2 = (int) (Math.random() * (max - min) + min);
-		int gen3 = (int) (Math.random() * (max - min) + min);
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
 
-		int minGen = Math.min(Math.min(gen1, gen2), gen3);
+        int gen1 = (int) (Math.random() * (max - min) + min);
+        int gen2 = (int) (Math.random() * (max - min) + min);
+        int gen3 = (int) (Math.random() * (max - min) + min);
 
-		System.out.println(gen1);
-		System.out.println(gen2);
-		System.out.println(gen3);
+        int minGen = Math.min(Math.min(gen1, gen2), gen3);
 
-		System.out.println("The minimal generated number was " + minGen);
-	}
+        System.out.println(gen1);
+        System.out.println(gen2);
+        System.out.println(gen3);
+
+        System.out.println("The minimal generated number was " + minGen);
+    }
 }
